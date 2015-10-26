4ada617a0caafa009a8eee2eeea27009c815072e
siphesande <siphesande@projectcodex.co>
2015-10-26 16:30:09 +0200

    python

--- mobile-app.py
+++ mobile-app.py
@@ -2,13 +2,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def mobile():
+def main():
+    return render_template('mobile.html')
+
+@app.route("/mtn_plan1")
+def MTN_Pay_Per_Second():
 
 
-	return render_template('mobile.html',
-		                    MTN_Pay_Per_Second= {"Peak SMS Rates" : "R 0.50",
+	return render_template('plans.html',
+		                    MTN_Pay_Per_Second={"Peak SMS Rates" : "R 0.50",
 		                                         "Call Rates In Network" : "R 0.70",
-		                                         "Call Rates Other Network" :"R 0.79"}
+		                                         "Call Rates Other Network" :"R 0.79",
+		                                         "Data Per MB" :"R 0.99"
+
+		                                         })
+
+@app.route("/plan1")	                        
+def MTN_zone():
+ return render_template('plan1.html',
+	                    MTN_Zone_Per_Second={"Peak SMS Rates" : "R 0.75",
+		                                         "Call Rates In Network" : "R 2.50",
+		                                         "Call Rates Other Network" : "R 2.50",
+		                                         "Data Per MB" : "R 2.00"
+		                                         })	       
 
    
 
