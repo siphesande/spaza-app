7b79781367ee3ee68887eb9290409ad6f47a64cd
siphesande <siphesande@projectcodex.co>
2015-11-20 15:39:17 +0200

    spaza-app

--- purchases.sql
+++ purchases.sql
@@ -2,6 +2,6 @@ INSERT INTO Purchases (Qty,Purchase_price,Purchase_date,Product_Id, Supplier_Id)
 SELECT stock_purchases_csv.quantity,stock_purchases_csv.cost,stock_purchases_csv.date, Products.Id AS product_id, Suppliers.Id AS supplier_id
 FROM stock_purchases_csv 
 INNER JOIN Suppliers
-ON Suppliers.Name = stock_purchases_csv.shop
+ON Suppliers.supplier_name = stock_purchases_csv.shop
 INNER JOIN Products
-ON Products.Name = stock_purchases_csv.item;
+ON Products.product_name = stock_purchases_csv.item;
