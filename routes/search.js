d821cf053a773c894eab0ccecc1bc011fbf2f144
siphesande <siphesande@projectcodex.co>
2016-03-29 13:36:24 +0200

    spaza-app

--- routes/search.js
+++ routes/search.js
@@ -1,28 +1,24 @@
 
 exports.searchProducts = function(req,res){
-  req.getConnection(function(err, connection){
-    var input = JSON.parse(JSON.stringify(req.body));
-      var key = input.key;
+   req.getConnection(function(err, connection){
+       var input = JSON.parse(JSON.stringify(req.body));
+       var key = input.key;
      
-       
+       if (err) return err;
+       connection.query('SELECT product_name from Products where product_name like "%'+req.query.key+'%"', function(err, rows, fields) {
+          if (err) throw err;
+          var data=[];
+          for(i=0;i<rows.length;i++)
+          {
+          data.push(rows[i].product_name);
+          }
+          JSON.stringify(data);
+          res.render('products',{
+          data: data
 
-
-
-    if (err) return err;
-    connection.query('SELECT product_name from Products where product_name like "%'+req.input.key+'%"', function(err, rows, fields) {
-    if (err) throw err;
-    var data=[];
-    for(i=0;i<rows.length;i++)
-      {
-        data.push(rows[i].product_name);
-      }
-      JSON.stringify(data);
-      res.render('products',{
-      data :data
-
-    });
-});
-
-
+          });
+       });
+  });
+}
 
 
